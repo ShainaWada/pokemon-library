ca4f207ffc6d360317291f1489505deb1bcde54d
Shaina Wada <shainah.wada09@gmail.com>
2023-09-14 22:53:32 +0900

    上限の変更

--- src/lib/pokemonAPI.ts
+++ src/lib/pokemonAPI.ts
@@ -1,7 +1,7 @@
 const POKEMON_API = "https://pokeapi.co/api/v2/";
-// getPokemonList
+
 export const getPokemonList = async () => {
-  const response = await fetch(POKEMON_API + "pokemon?limit=151&offset=0");
+  const response = await fetch(POKEMON_API + "pokemon?limit=500&offset=0");
   const data = await response.json();
   return data.results;
 };
