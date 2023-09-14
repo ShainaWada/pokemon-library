8b5c4ea183d98057700a29c437ad21ed36a9e559
Shaina Wada <shainah.wada09@gmail.com>
2023-09-14 22:52:41 +0900

    不要なインポートの削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
-import { PokemonCard } from '@/components/PokemonCard'
 import { PokemonGrid } from '@/components/pokemonGrid'
-import Image from 'next/image'
-
 import { getPokemonList } from '@/lib/pokemonAPI';
 
 export default async function Home() {
